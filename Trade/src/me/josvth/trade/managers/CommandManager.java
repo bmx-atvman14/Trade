2453f8d60496dc61bab9e36d8f545a84a0acdc1a
Josvth <josvth@live.nl>
2013-01-19 22:21:39 +0100

    Added the ability to accept trades using /trade <nameofrequester> added a /trade listen command to cancel ignoring.

--- Trade/src/me/josvth/trade/managers/CommandManager.java
+++ Trade/src/me/josvth/trade/managers/CommandManager.java
@@ -1,6 +1,7 @@
 package me.josvth.trade.managers;
 
 import me.josvth.trade.Trade;
+import static me.josvth.trade.managers.LanguageManager._s;
 import me.josvth.trade.managers.RequestManager.RequestMethod;
 import me.josvth.trade.managers.RequestManager.RequestRestriction;
 
@@ -36,12 +37,12 @@ public class CommandManager implements CommandExecutor {
 			if(args[0].equalsIgnoreCase("ignore")){
 
 				if(!sender.hasPermission("trade.ignore")){
-					languageManager.sendMessage(sender, "command.no-permission");
+					_s(sender, "command.no-permission");
 					return true;
 				}
 
 				if(!(sender instanceof Player)){ 
-					languageManager.sendMessage(sender, "command.only-player");
+					_s(sender, "command.only-player");
 					return true;
 				}
 
@@ -49,239 +50,190 @@ public class CommandManager implements CommandExecutor {
 
 				requestManager.toggleIgnoring( player );
 
-
 				return true;
 			}
 
+			// /trade listen
+			if(args[0].equalsIgnoreCase("listen")){
+
+				if(!sender.hasPermission("trade.ignore")){
+					_s(sender, "command.no-permission");
+					return true;
+				}
+
+				if(!(sender instanceof Player)){ 
+					_s(sender, "command.only-player");
+					return true;
+				}
+
+				Player player = (Player)sender;
+
+				requestManager.listenAll( player );
+				
+				return true;
+			}
+			
 			// /trade reload
 			if(args[0].equalsIgnoreCase("reload")){
 				if(!sender.hasPermission("trade.reload")){
-					languageManager.sendMessage(sender, "command.no-permission");
+					_s(sender, "command.no-permission");
 				}else{
 
 					plugin.getLanguageManager().reload();
 					plugin.getConfigurationManager().reload();
 					plugin.getLanguageManager().reload();
 
-					languageManager.sendMessage(sender, "global.reload");
+					_s(sender, "global.reload");
 				}
 				return true;
 			}
 
-			// /trade <player>
+			// /trade <player> // accept or make new
+
 			if(!(sender instanceof Player)){ 
-				languageManager.sendMessage(sender, "command.only-player");
+				_s(sender, "command.only-player");
 				return true;
 			}
 
 			Player player = (Player)sender;
 
 			Player requested = plugin.getServer().getPlayer(args[0]);
+
 			if(requested == null){
-				languageManager.sendMessage( player, "request.player-not-found", new String[][]{ {"%playername%", args[0]} });
+				_s( player, "request.player-not-found", new String[][]{ {"%playername%", args[0]} });
 				return true;
 			}
 
+			boolean acceptation = requestManager.isRequested( requested, player );
+			
 			RequestRestriction restriction = requestManager.mayRequest( player , requested, RequestMethod.COMMAND );
 
-			switch ( restriction ) {
-			case NO_PERMISSION:
-				languageManager.sendMessage( player, "command.no-permission");
-				break;
-			case CROSS_WORLD:
-				languageManager.sendMessage( player, "request.no-cross-world");
-				break;	
-			case CROSS_GAMEMODE:
-				languageManager.sendMessage( player, "request.no-cross-gamemode");			
-				break;
-			case IN_TRADE:
-				languageManager.sendMessage( player, "request.in-trade");			
-				break;
-			case IGNORING:
-				languageManager.sendMessage( player, "request.ignoring.is-ignoring", new String[][]{ {"%playername%", requested.getName()} } );			
-				break;
-			case REQUESTER_WORLD:
-				languageManager.sendMessage( player, "request.disabled-world.player");			
-				break;
-			case REQUESTED_WORLD:
-				languageManager.sendMessage( player, "request.disabled-world.requested", new String[][]{ {"%playername%", requested.getName()} });			
-				break;
-			case OUT_OF_RANGE:
-				languageManager.sendMessage( player, "request.out-of-range", new String[][]{ {"%playername%", requested.getName()} });			
-				break;
-			case NO_SIGHT:
-				languageManager.sendMessage( player, "request.must-see");			
-				break;
-			case WAIT:
-				languageManager.sendMessage( player, "request.please-wait");			
-				break;
-			case REQUESTER_MOBARENA:
-				languageManager.sendMessage( player, "request.mobarena.player");			
-				break;
-			case REQUESTED_MOBARENA:
-				languageManager.sendMessage( player, "request.mobarena.requested", new String[][]{ {"%playername%", requested.getName()} });			
-				break;
-			case CITIZEN:
-				languageManager.sendMessage( player, "request.citizen");			
-				break;
-			case REQUESTER_REGION:
-				languageManager.sendMessage( player, "request.region.player");			
-				break;
-			case REQUESTED_REGION:
-				languageManager.sendMessage( player, "request.region.requested", new String[][]{ {"%playername%", requested.getName()} });			
-				break;
-			default:
-				requestManager.makeRequest( player, requested );
-				break;
+			if ( !RequestRestriction.ALLOW.equals( restriction ) ) {
+				sendMayRequestResponse( player, requested, restriction );
+				return true;
 			}
 
+			if ( acceptation ) 
+				requestManager.acceptRequest( player, requested);
+			else
+				requestManager.makeRequest( player, requested );
+
 			return true;
+
 		}
 
 		if(args.length == 2){
-
-			// /trade accept <player>
-			if (args[0].equalsIgnoreCase("accept")){
-
-				if(!(sender instanceof Player)){ 
-					languageManager.sendMessage(sender, "command.only-player");
-					return true;
-				}
-
-				
-				Player player = (Player)sender;
-				Player requester = plugin.getServer().getPlayer(args[1]);
-				
-				if(requester == null){
-					languageManager.sendMessage( player, "request.player-not-found", new String[][]{ {"%playername%", args[0]} });
-					return true;
-				}
-				
-				RequestRestriction restriction = requestManager.mayRequest( player , requester, RequestMethod.COMMAND );
-
-				switch ( restriction ) {
-				case NO_PERMISSION:
-					languageManager.sendMessage( player, "command.no-permission");
-					break;
-				case CROSS_WORLD:
-					languageManager.sendMessage( player, "request.no-cross-world");
-					break;	
-				case CROSS_GAMEMODE:
-					languageManager.sendMessage( player, "request.no-cross-gamemode");			
-					break;
-				case IN_TRADE:
-					languageManager.sendMessage( player, "request.in-trade");			
-					break;
-				case IGNORING:
-					languageManager.sendMessage( player, "request.ignoring.is-ignoring", new String[][]{ {"%playername%", requester.getName()} } );			
-					break;
-				case REQUESTER_WORLD:
-					languageManager.sendMessage( player, "request.disabled-world.player");			
-					break;
-				case REQUESTED_WORLD:
-					languageManager.sendMessage( player, "request.disabled-world.requested", new String[][]{ {"%playername%", requester.getName()} });			
-					break;
-				case NO_SIGHT:
-					languageManager.sendMessage( player, "request.must-see");			
-					break;
-				case WAIT:
-					languageManager.sendMessage( player, "request.please-wait");			
-					break;
-				case REQUESTER_MOBARENA:
-					languageManager.sendMessage( player, "request.mobarena.player");			
-					break;
-				case REQUESTED_MOBARENA:
-					languageManager.sendMessage( player, "request.mobarena.requested", new String[][]{ {"%playername%", requester.getName()} });			
-					break;
-				case CITIZEN:
-					languageManager.sendMessage( player, "request.citizen");			
-					break;
-				case REQUESTER_REGION:
-					languageManager.sendMessage( player, "request.region.player");			
-					break;
-				case REQUESTED_REGION:
-					languageManager.sendMessage( player, "request.region.requested", new String[][]{ {"%playername%", requester.getName()} });			
-					break;
-				default:
-					requestManager.acceptRequest( player, requester );
-					break;
-				}
-
-				return true;
-				
-			}
 
 			// /trade refuse <player>
 			if (args[0].equalsIgnoreCase("refuse")){
 
 				if(!(sender instanceof Player)){ 
-					languageManager.sendMessage(sender, "command.only-player");
+					_s(sender, "command.only-player");
 					return true;
 				}
 
-				
+
 				Player player = (Player)sender;
 				Player requester = plugin.getServer().getPlayer(args[1]);
-				
+
 				if(requester == null){
-					languageManager.sendMessage( player, "request.player-not-found", new String[][]{ {"%playername%", args[0]} });
+					_s( player, "request.player-not-found", new String[][]{ {"%playername%", args[0]} });
 					return true;
 				}
 				
 				RequestRestriction restriction = requestManager.mayRequest( player , requester, RequestMethod.COMMAND );
-				switch ( restriction ) {
-				case NO_PERMISSION:
-					languageManager.sendMessage( player, "command.no-permission");
-					break;
-				case CROSS_WORLD:
-					languageManager.sendMessage( player, "request.no-cross-world");
-					break;	
-				case CROSS_GAMEMODE:
-					languageManager.sendMessage( player, "request.no-cross-gamemode");			
-					break;
-				case IN_TRADE:
-					languageManager.sendMessage( player, "request.in-trade");			
-					break;
-				case IGNORING:
-					languageManager.sendMessage( player, "request.ignoring.is-ignoring", new String[][]{ {"%playername%", requester.getName()} } );			
-					break;
-				case REQUESTER_WORLD:
-					languageManager.sendMessage( player, "request.disabled-world.player");			
-					break;
-				case REQUESTED_WORLD:
-					languageManager.sendMessage( player, "request.disabled-world.requested", new String[][]{ {"%playername%", requester.getName()} });			
-					break;
-				case NO_SIGHT:
-					languageManager.sendMessage( player, "request.must-see");			
-					break;
-				case WAIT:
-					languageManager.sendMessage( player, "request.please-wait");			
-					break;
-				case REQUESTER_MOBARENA:
-					languageManager.sendMessage( player, "request.mobarena.player");			
-					break;
-				case REQUESTED_MOBARENA:
-					languageManager.sendMessage( player, "request.mobarena.requested", new String[][]{ {"%playername%", requester.getName()} });			
-					break;
-				case CITIZEN:
-					languageManager.sendMessage( player, "request.citizen");			
-					break;
-				case REQUESTER_REGION:
-					languageManager.sendMessage( player, "request.region.player");			
-					break;
-				case REQUESTED_REGION:
-					languageManager.sendMessage( player, "request.region.requested", new String[][]{ {"%playername%", requester.getName()} });			
-					break;
-				default:
-					requestManager.refuseRequest( player, requester );
-					break;
+
+				if ( !RequestRestriction.ALLOW.equals( restriction ) ) {
+					sendMayRequestResponse( player, requester, restriction );
+					return true;
 				}
-				
+
+				requestManager.refuseRequest( player, requester );
+
 				return true;
+				
 			}
 		}
 
+		// /trade accept <player>
+		if (args[0].equalsIgnoreCase("accept") ){
+
+			if(!(sender instanceof Player)){ 
+				_s(sender, "command.only-player");
+				return true;
+			}
+
+			Player player = (Player)sender;
+			Player requester = plugin.getServer().getPlayer(args[1]);
+
+			if(requester == null){
+				_s( player, "request.player-not-found", new String[][]{ {"%playername%", args[0]} });
+				return true;
+			}
+
+			RequestRestriction restriction = requestManager.mayRequest( player , requester, RequestMethod.COMMAND );
+
+			if ( !RequestRestriction.ALLOW.equals( restriction ) ) {
+				sendMayRequestResponse( player, requester, restriction );
+				return true;
+			}
+
+			requestManager.acceptRequest( player, requester );
+			
+		}
+
 		return false;
+
+	}
+	
+	private void sendMayRequestResponse( Player player, Player requester, RequestRestriction restriction ) {
+		
+		switch ( restriction ) {
+		case NO_PERMISSION:
+			_s( player, "command.no-permission");
+			break;
+		case CROSS_WORLD:
+			_s( player, "request.no-cross-world");
+			break;	
+		case CROSS_GAMEMODE:
+			_s( player, "request.no-cross-gamemode");			
+			break;
+		case IN_TRADE:
+			_s( player, "request.in-trade");			
+			break;
+		case IGNORING:
+			_s( player, "request.ignoring.is-ignoring", new String[][]{ {"%playername%", requester.getName()} } );			
+			break;
+		case REQUESTER_WORLD:
+			_s( player, "request.disabled-world.player");			
+			break;
+		case REQUESTED_WORLD:
+			_s( player, "request.disabled-world.requested", new String[][]{ {"%playername%", requester.getName()} });			
+			break;
+		case NO_SIGHT:
+			_s( player, "request.must-see");			
+			break;
+		case WAIT:
+			_s( player, "request.please-wait");			
+			break;
+		case REQUESTER_MOBARENA:
+			_s( player, "request.mobarena.player");			
+			break;
+		case REQUESTED_MOBARENA:
+			_s( player, "request.mobarena.requested", new String[][]{ {"%playername%", requester.getName()} });			
+			break;
+		case CITIZEN:
+			_s( player, "request.citizen");			
+			break;
+		case REQUESTER_REGION:
+			_s( player, "request.region.player");			
+			break;
+		case REQUESTED_REGION:
+			_s( player, "request.region.requested", new String[][]{ {"%playername%", requester.getName()} });			
+			break;
+		default:
+			break;
+		}
 		
 	}
 }
