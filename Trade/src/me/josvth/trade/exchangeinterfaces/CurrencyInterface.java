8924b88d924c40c9d99cfd85463193f570061e92
Josvth <josvth@live.nl>
2013-01-20 17:10:58 +0100

    Let the currency display items show the amount of money added to the trade.

--- Trade/src/me/josvth/trade/exchangeinterfaces/CurrencyInterface.java
+++ Trade/src/me/josvth/trade/exchangeinterfaces/CurrencyInterface.java
@@ -1,16 +1,22 @@
 package me.josvth.trade.exchangeinterfaces;
 
+import java.util.regex.Matcher;
+
 import me.josvth.trade.layouts.CurrencyLayout;
+import static me.josvth.trade.managers.LanguageManager._;
+import static me.josvth.trade.managers.ExtensionManager.economy;
 
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
 
 public class CurrencyInterface extends ItemInterface {
 
 	int leftAmount = 0; 
 	int rightAmount = 0;
-	
+
 	CurrencyLayout layout;
-	
+
 	public CurrencyInterface( CurrencyLayout layout, String otherPlayerName ) {
 		super( otherPlayerName, layout );
 		this.layout = layout;
@@ -34,37 +40,48 @@ public class CurrencyInterface extends ItemInterface {
 		int medium 	= 	(int) 	Math.floor( ( amount - getLayout().getLargeCurrency() * large ) / getLayout().getMediumCurrency() );
 		int small 	= 	(int) 	Math.floor( ( amount - getLayout().getLargeCurrency() * large - getLayout().getMediumCurrency() * medium ) ) / getLayout().getSmallCurrency();
 
-		ItemStack largeItem = getLayout().getLargeItemDisplay();
+		ItemStack largeItem = getLayout().getLargeDisplayItem().clone();
 		largeItem.setAmount( large );
-
-		ItemStack mediumItem = getLayout().getMediumItemDisplay();
+			
+		ItemStack mediumItem = getLayout().getMediumDisplayItem().clone();
 		mediumItem.setAmount( medium );
-
-		ItemStack smallItem = getLayout().getSmallItemDisplay();
+	
+		ItemStack smallItem = getLayout().getSmallDisplayItem().clone();
 		smallItem.setAmount( small );
-
 		
+		ItemMeta largeMeta = largeItem.getItemMeta();
+		ItemMeta mediumMeta = mediumItem.getItemMeta();
+		ItemMeta smallMeta = smallItem.getItemMeta();
+
 		switch ( side ){
 		case LEFT:
 
+			String labelLeft = _( "trade.items.currency.labels.display.left", new String[][]{ { "%amount%", Matcher.quoteReplacement( economy.format( amount ) ) } } );
+
 			for ( int slot : getLayout().getLeftDisplayLargeSlots() ) {
-				if (large > 0)
+				if (large > 0) {
+					largeMeta.setDisplayName( labelLeft );
+					largeItem.setItemMeta( largeMeta );
 					inventory.setItem( slot , largeItem ) ;
-				else 
+				} else 
 					inventory.clear( slot );
 			}
 
 			for ( int slot : getLayout().getLeftDisplayMediumSlots() ) {
-				if (medium > 0) 
+				if (medium > 0) {
+					mediumMeta.setDisplayName( labelLeft );
+					mediumItem.setItemMeta( mediumMeta );
 					inventory.setItem( slot , mediumItem ) ;
-				else 
+				} else 
 					inventory.clear( slot );
 			}
 
 			for ( int slot : getLayout().getLeftDisplaySmallSlots() ) {
-				if (small > 0)
+				if (small > 0) {
+					smallMeta.setDisplayName( labelLeft );
+					smallItem.setItemMeta( smallMeta );
 					inventory.setItem( slot , smallItem ) ;
-				else 
+				} else 
 					inventory.clear( slot );
 			}
 
@@ -73,25 +90,41 @@ public class CurrencyInterface extends ItemInterface {
 			break;
 
 		case RIGHT:
+			
+			largeMeta.setLore( null );	// TODO do this somewhere else (maybe adding another item)?
+			mediumMeta.setLore( null );	// TODO do this somewhere else (maybe adding another item)?
+			smallMeta.setLore( null );	// TODO do this somewhere else (maybe adding another item)?
+
+			String labelRight = 
+			_( "trade.items.currency.labels.display.right", new String[][]{ 
+					{ "%playername%", getNameOtherPlayer() },
+					{ "%amount%", Matcher.quoteReplacement( economy.format( amount ) ) }
+			} );
 
 			for ( int slot : getLayout().getRightDisplayLargeSlots() ) {
-				if (large > 0) 
+				if (large > 0) {
+					largeMeta.setDisplayName( labelRight );
+					largeItem.setItemMeta( largeMeta );
 					inventory.setItem( slot , largeItem ) ;
-				else 
+				} else 
 					inventory.clear( slot );
 			}
 
 			for ( int slot : getLayout().getRightDisplayMediumSlots() ) {
-				if (medium > 0) 
+				if (medium > 0) {
+					mediumMeta.setDisplayName( labelRight );
+					mediumItem.setItemMeta( mediumMeta );
 					inventory.setItem( slot , mediumItem ) ;
-				else 
+				} else 
 					inventory.clear( slot );
 			}
 
 			for ( int slot : getLayout().getRightDisplaySmallSlots() ) {
-				if (small > 0)
+				if (small > 0) {
+					smallMeta.setDisplayName( labelRight );
+					smallItem.setItemMeta( smallMeta );
 					inventory.setItem( slot , smallItem ) ;
-				else 
+				} else 
 					inventory.clear( slot );
 			}
 
@@ -105,15 +138,15 @@ public class CurrencyInterface extends ItemInterface {
 
 	// Returns the amount represented by the currency slot 
 	public int getCurrencySlot(int slot) {
-		for ( int small 	: getLayout().getLeftSmallSlots() 	) if ( small 	== slot ) return getLayout().getSmallCurrency();
-		for ( int medium 	: getLayout().getLeftMediumSlots() 	) if ( medium 	== slot ) return getLayout().getMediumCurrency();
-		for ( int large 	: getLayout().getLeftLargeSlots() 	) if ( large 	== slot ) return getLayout().getLargeCurrency();
-		
+		for ( int small 	: getLayout().getChangeSmallSlots() 	) if ( small 	== slot ) return getLayout().getSmallCurrency();
+		for ( int medium 	: getLayout().getChangeMediumSlots() 	) if ( medium 	== slot ) return getLayout().getMediumCurrency();
+		for ( int large 	: getLayout().getChangeLargeSlots() 	) if ( large 	== slot ) return getLayout().getLargeCurrency();
+
 		for ( int smallD 	: getLayout().getLeftDisplaySmallSlots() 	) if ( smallD 	== slot ) return -1*getLayout().getSmallCurrency();
 		for ( int mediumD 	: getLayout().getLeftDisplayMediumSlots() 	) if ( mediumD 	== slot ) return -1*getLayout().getMediumCurrency();
 		for ( int largeD 	: getLayout().getLeftDisplayLargeSlots() 	) if ( largeD 	== slot ) return -1*getLayout().getLargeCurrency();
-		
+
 		return 0;
 	}
-	
+
 }
